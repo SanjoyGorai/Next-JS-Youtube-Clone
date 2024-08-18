6fe3b92044a55b141877f9e4a917925d9a8f5928
Mr. Sanjoy <--add>
2024-08-18 08:42:08 +0530

    component folder, styles folder are created

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Youtube Clone with Next Js",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
+      <link rel="icon" href="youtube.svg" />
+    </html>
+  );
+}
